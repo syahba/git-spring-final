a21c8fbc10b7a3fcc190824d0092f5ef7b2ed913
syahba <ksyaahba@gmail.com>
2024-06-04 14:17:18 +0700

    initial commit

---
+++ src/main/java/com/example/assignment_4/controller/AuthController.java
@@ -0,0 +1,5 @@
+package com.example.assignment_4.controller;
+
+public class AuthController {
+  
+}
---
+++ src/main/java/com/example/assignment_4/repository/AgencyRepository.java
@@ -0,0 +1,5 @@
+package com.example.assignment_4.repository;
+
+public interface AgencyRepository {
+  
+}
